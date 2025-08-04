aa0157bb8de58b4baf785a0099319813543ca91e
andy darmawan <andy.daz.95@gmail.com>
2025-08-04 10:14:01 +0700

    add journal page

--- resources/js/components/pages/Journal.js
+++ resources/js/components/pages/Journal.js
@@ -17,29 +17,87 @@ const Journal = () => {
         // setData(response);
     };
 
+    // const columns = React.useMemo(
+    //     () => [
+    //         {
+    //             Header: "Tanggal",
+    //             accessor: "date",
+    //         },
+    //         {
+    //             Header: "Keterangan",
+    //             accessor: "description",
+    //         },
+    //         {
+    //             Header: "debit",
+    //             accessor: "debit",
+    //         },
+    //         {
+    //             Header: "kredit",
+    //             accessor: "credit",
+    //         },
+    //     ],
+    //     []
+    // );
+
+    const flattenJournalData = (data) => {
+        let rows = [];
+        data.forEach((batch) => {
+            rows.push({
+                ...batch,
+                isBatch: true,
+            });
+            batch.entries.forEach((entry) => {
+                rows.push({
+                    ...entry,
+                    isBatch: false,
+                });
+            });
+        });
+        return rows;
+    };
+
     const columns = React.useMemo(
         () => [
             {
                 Header: "Tanggal",
-                accessor: "date",
+                accessor: (row) => (row.isBatch ? row.date : ""),
             },
             {
                 Header: "Keterangan",
-                accessor: "description",
+                accessor: (row) => row.description,
+                className: "text-left",
+                Cell: ({ value, row }) => (
+                    <div
+                        style={{
+                            textAlign: "left",
+                            paddingLeft: row.original.isBatch ? "0" : "20px",
+                        }}
+                    >
+                        {value}
+                    </div>
+                ),
             },
             {
                 Header: "debit",
-                accessor: "debit",
+                accessor: (row) => (row.isBatch ? "" : row.debit),
+                className: "text-right",
+                Cell: ({ value }) => (
+                    <div style={{ textAlign: "right" }}>{value}</div>
+                ),
             },
             {
                 Header: "kredit",
-                accessor: "credit",
+                accessor: (row) => (row.isBatch ? "" : row.credit),
+                className: "text-right",
+                Cell: ({ value }) => (
+                    <div style={{ textAlign: "right" }}>{value}</div>
+                ),
             },
         ],
         []
     );
 
-    const tableInstance = useTable({ columns, data: data });
+    const tableInstance = useTable({ columns, data: flattenJournalData(data) });
 
     useEffect(() => {
         getJournal();
